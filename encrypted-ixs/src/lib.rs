84b0cbcdc06015d551974214d7a83b1dcf9988b9
tilakkumar56 <73422064+tilakkumar56@users.noreply.github.com>
2026-03-22 16:08:55 +0000

    Reduce to 2 features to fit Solana tx limit with all Arcium accounts

--- encrypted-ixs/src/lib.rs
+++ encrypted-ixs/src/lib.rs
@@ -4,7 +4,7 @@ use arcis::*;
 mod circuits {
     use arcis::*;
 
-    const TEMPLATE_SIZE: usize = 4;
+    const TEMPLATE_SIZE: usize = 2;
 
     pub struct BiometricTemplate {
         features: [u128; TEMPLATE_SIZE],
