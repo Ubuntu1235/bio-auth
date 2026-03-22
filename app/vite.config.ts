8882d682c527e3bf975f560454f2a925deb28ba8
tilakkumar56 <73422064+tilakkumar56@users.noreply.github.com>
2026-03-22 18:47:22 +0000

    Add base path for GitHub Pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig({ base: "/bio-auth/",
   plugins: [
     react(),
     nodePolyfills({
